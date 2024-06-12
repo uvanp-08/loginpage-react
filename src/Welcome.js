6d7fb69d338a731368f19c8c9d23a83c29446483
N P Yuvashree <139610976+uvanp-08@users.noreply.github.com>
2024-06-13 01:11:45 +0530

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,16 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function Welcome({ email }) {
+  const handleLogout = () => {
+    localStorage.removeItem('email');
+    window.location.reload();
+  };
+
   return (
     <div>
       <h2 className="welcome">Welcome, {email}!</h2>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
